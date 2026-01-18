35c7ed1ad0082a82a9123c6a04de2b1ceb513234
bluelhf <ilari.suhonen@gmail.com>
2026-01-19 00:24:23 +0200

    fix(balloons): make balloons more stubborn

--- components/balloons/hooks/balloons.tsx
+++ components/balloons/hooks/balloons.tsx
@@ -16,7 +16,7 @@ export default function useBalloons() {
 
             const wind = (balloon: Balloon) => ({
                 dx: Math.sin(now / 2000) * 0.005,
-                dy: Math.abs(balloon.vy) * -0.001 * Math.random()
+                dy: Math.abs(balloon.vy) * -0.001 * Math.random() - 0.2
             });
 
             for (let i = balloons.length - 1; i >= 0; i--) {

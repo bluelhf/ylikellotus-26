e271cf3f51fde990591e6455166e81d8bdb2aa1c
bluelhf <ilari.suhonen@gmail.com>
2026-01-21 13:45:51 +0200

    fix(office): revert broken changes

--- components/OfficeScene.tsx
+++ components/OfficeScene.tsx
@@ -17,18 +17,17 @@ export default function OfficeScene() {
     // bypass react here for performance reasons
     const officeRef = useRef<HTMLDivElement>(null);
     const cubicleRef = useRef<HTMLDivElement>(null);
-    const createScrollListener = (mdBreakpointSnapshot: boolean) => () => {
+    const createScrollListener = () => () => {
         const position = window.pageYOffset;
         if (officeRef.current) officeRef.current.style.transform = `translateX(-50%) translateY(${position * 0.7}px)`;
         if (cubicleRef.current) {
             cubicleRef.current.style.transform = `translateX(-50%) translateY(${position * 0.5}px)`;
-            cubicleRef.current.style.top = `${(mdBreakpointSnapshot ? 500 : 300)}px`;
         }
     };
 
     useEffect(() => {
         // called whenever we pass the md breakpoint threshold (for example, because of resize)
-        const listener = createScrollListener(hasMdBreakpoint);
+        const listener = createScrollListener();
         window.addEventListener("scroll", listener, { passive: true });
 
         return () => {
@@ -38,7 +37,7 @@ export default function OfficeScene() {
 
     return (
         <Scene className="relative">
-            <div ref={officeRef} className="will-change-transform relative scene-body left-1/2 -translate-x-1/2" style={{ transform: 'translateX(-50%)' }}>
+            <div ref={officeRef} className="will-change-transform relative scene-body left-1/2 -translate-x-1/2 !min-w-[1200px] md:!min-w-[2500px]" style={{ transform: 'translateX(-50%)' }}>
                 <div className="grid place-items-center">
                     <ExportedImage alt="Toimisto" src={toimistoImg} className="col-start-1 row-start-1" basePath={nextConfig.basePath} />
                     <div className="grid col-start-1 row-start-1 !min-h-[0.85em] md:!min-h-[1.7em] !min-w-[9%] -mb-[-29.8%] ml-[1.1%]">
@@ -46,7 +45,7 @@ export default function OfficeScene() {
                     </div>
                 </div>
             </div>
-            <div ref={cubicleRef} className="absolute scene-body top-[300px] md:top-[500px] left-1/2 -translate-x-1/2" style={{ transform: 'translateX(-50%)' }}>
+            <div ref={cubicleRef} className="absolute scene-body top-[300px] md:top-[500px] left-1/2 -translate-x-1/2 !min-w-[1200px] md:!min-w-[2500px]" style={{ transform: 'translateX(-50%)' }}>
                 <ExportedImage alt="Cubicles" src={cubiclesImg} basePath={nextConfig.basePath}/>
             </div>
         </Scene>
